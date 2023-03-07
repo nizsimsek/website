71ec21f04fbe4a88a97634ef7ddc4f61ee2a61a7
Nizamettin Şimşek <nizsimsek@icloud.com>
2023-03-07 22:19:08 +0300

    Build Error Fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 import { useSelectedLayoutSegment } from "next/navigation";
 
-export default function Footer({ menuIsOpen }) {
+export default function Footer({ menuIsOpen }: { menuIsOpen: boolean }) {
   const activeSegment = useSelectedLayoutSegment();
   return (
     <>
