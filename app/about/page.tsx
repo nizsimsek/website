06e5c4615f5d62e2b4e82658f92b49214b3a54d9
Nizamettin Şimşek <nizsimsek@icloud.com>
2023-04-11 16:51:06 +0300

    About Page Gif

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function AboutMe() {
     return (
         <div className="flex h-full w-full">
-            <Image src="https://www.reactiongifs.us/wp-content/uploads/2017/12/giphy-1.gif" alt="Coding" width={300} height={200} className="w-1/2 h-1/2 m-auto" />
+            <Image src="https://www.reactiongifs.us/wp-content/uploads/2017/12/giphy-1.gif" alt="Coding" width={300} height={200} className="w-1/2 m-auto object-contain" />
         </div>
     );
 }
